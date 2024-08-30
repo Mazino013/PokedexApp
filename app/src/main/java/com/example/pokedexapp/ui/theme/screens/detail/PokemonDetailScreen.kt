5daf3bcdf4fb8d0c7332d56ae55500599858764a
Zino <ewomazinoefe@gmail.com>
2024-08-30 19:25:54 -0400

    Cleaned up the UI & Refresh Detail Screen

--- app/src/main/java/com/example/pokedexapp/ui/theme/screens/detail/PokemonDetailScreen.kt
+++ app/src/main/java/com/example/pokedexapp/ui/theme/screens/detail/PokemonDetailScreen.kt
@@ -17,7 +17,6 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -26,9 +25,7 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
