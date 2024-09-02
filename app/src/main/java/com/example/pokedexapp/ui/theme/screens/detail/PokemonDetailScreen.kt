2b3b97eec0e7cab807c6d9af9348f1d0ae4e1fc0
Zino <ewomazinoefe@gmail.com>
2024-09-01 20:20:09 -0400

    Clean up UI for Chips and stack them in a row in detail screen: Capitalize P.Name

--- app/src/main/java/com/example/pokedexapp/ui/theme/screens/detail/PokemonDetailScreen.kt
+++ app/src/main/java/com/example/pokedexapp/ui/theme/screens/detail/PokemonDetailScreen.kt
@@ -26,15 +26,12 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import com.example.pokedexapp.util.ColorUtils
-import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
 
 @Composable
 fun PokemonDetailScreen(viewModel: PokemonDetailViewModel) {
